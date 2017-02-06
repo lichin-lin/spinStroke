3bc704f440698ca3402abf39132b452cf3810560
Jim <vic20087cjimlin@gmail.com>
2017-02-07 00:07:27 +0800

    move (fonts/ , sass/) and finish header component

--- src/js/components/Base.jsx
+++ src/js/components/Base.jsx
@@ -1,5 +1,5 @@
-import React, { Component, PropTypes } from 'react'
-// import Containers from 'js/containers'
+import React, { Component } from 'react'
+import Containers from 'js/containers'
 import CSSModules from 'react-css-modules'
 // import {
 //     Grid,
@@ -7,24 +7,10 @@ import CSSModules from 'react-css-modules'
 // } from 'react-bootstrap'
 
 export default CSSModules(class Base extends Component {
-    static propTypes = {
-        requiredProps: PropTypes.string.isRequired,
-        defaultProps: PropTypes.string
-    };
-    static defaultProps = {
-        defaultProps: 'default props'
-    };
     render () {
         return (
             <div id="wrapper">
-                <header id="header" className="alt">
-                    {/* <Containers.section.Header /> */}
-                    <span className="logo"><img src="images/logo.svg" alt="" /></span>
-                    <h1>Stellar</h1>
-                    <p>Just another free, fully responsive site template<br />
-                    built by <a href="https://twitter.com/ajlkn">@ajlkn</a> for <a href="https://html5up.net">HTML5 UP</a>.</p>
-                </header>
-
+                <Containers.section.Header />
                 <nav id="nav">
                     <ul>
                         <li><a href="#intro" className="active">Introduction</a></li>
@@ -33,10 +19,10 @@ export default CSSModules(class Base extends Component {
                         <li><a href="#cta">Get Started</a></li>
                     </ul>
                 </nav>
-
                 <div id="main"></div>
-                <footer id="footer"></footer>
+                <footer id="footer">
+                </footer>
             </div>
         )
     }
-}, require('./sass/main.scss'))
+}, require('./../../sass/main.scss'))
--- src/js/components/section/Header.jsx
+++ src/js/components/section/Header.jsx
@@ -3,9 +3,12 @@ import React, { Component } from 'react'
 export default class Header extends Component {
     render () {
         return (
-            <div>
-                XD
-            </div>
+            <header id="header" className="alt">
+                <span className="logo"><img src="images/logo.svg" alt="" /></span>
+                <h1>SpinStroke</h1>
+                <p>A spining stroke animation with morphing effect!<br />
+                built by <a href="https://web.poe.garena.tw/account/view-profile/TocknicSu">@tocknicsu</a>.</p>
+            </header>
         )
     }
 }

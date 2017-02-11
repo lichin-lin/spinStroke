e7e437dcaaadc9839db13346a044453755e3bed7
Tocknicsu <wingemerald@gmail.com>
2017-02-11 21:34:04 +0800

    complete iframe url parsing

--- src/js/components/Iframe.jsx
+++ src/js/components/Iframe.jsx
@@ -5,9 +5,23 @@ export default class extends Component {
     componentDidMount () {
     }
     render () {
+        /*
+        var obj = {
+            height: 700,
+            width: 1200,
+            texts: ['gg', 'wp']
+        }
+
+        var str = JSON.stringify(obj)
+
+        console.log(new Buffer(str).toString('base64'))
+        console.log(this.props.location.query.data)
+        */
+        var obj = JSON.parse(new Buffer(this.props.location.query.data, 'base64').toString('ascii'))
+
         return (
             <div>
-                <Containers.stroke/>
+                <Containers.stroke {...obj}/>
                 { process.env.NODE_ENV !== 'production' ? <Containers.DevTools/> : null }
             </div>
         )

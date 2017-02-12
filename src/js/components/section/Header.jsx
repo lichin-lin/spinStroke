fb1a866c67b1bd7051e9cf239b0479e3d55a5596
Tocknicsu <wingemerald@gmail.com>
2017-02-12 21:37:27 +0800

    fixed tocknicsu link

--- src/js/components/section/Header.jsx
+++ src/js/components/section/Header.jsx
@@ -7,7 +7,7 @@ export default class Header extends Component {
                 <span className="logo"><img src="images/logo.svg" alt="" /></span>
                 <h1>SpinStroke</h1>
                 <p>A spining stroke animation with morphing effect!<br />
-                built by <a href="https://web.poe.garena.tw/account/view-profile/TocknicSu">@tocknicsu</a> and <a href="https://github.com/lichin-lin">@lichin-lin</a></p>
+                built by <a href="https://github.com/Tocknicsu">@tocknicsu</a> and <a href="https://github.com/lichin-lin">@lichin-lin</a></p>
             </header>
         )
     }
